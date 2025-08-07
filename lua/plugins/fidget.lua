295bd8b3cbe19cf8bc027a3c7f6a25686d421230
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-08-07 16:56:46 +0800

    add nvim configs w/ lazy - autotag.lua - blink.lua - conform.lua - copilot.lua - fidget.lua - gitsigns.lua - tokyonight.lua - treesitter.lua - trouble.lua - which-key.lua - lsp.lua - lualine.lua - nvim-tree.lua - oil.lua - render-markdown.lua - telescope.lua

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,15 +1,18 @@
 return {
     "j-hui/fidget.nvim",
-    config = function()
-        require("fidget").setup({
+    opts = {
+        notification = {
             window = {
                 border = "rounded",
+                x_padding = 1,
+                y_padding = 1,
             },
-            integration = {
-                ["nvim-tree"] = {
-                    enable = true, -- Integrate with nvim-tree/nvim-tree.lua (if installed)
-                },
+        },
+        integration = {
+            ["nvim-tree"] = {
+                enable = true, -- Integrate with nvim-tree/nvim-tree.lua (if installed)
             },
-        })
-    end
+        },
+
+    }
 }
